caa7609d26adbbfc4f61017a6ef7d64ec9166075
Biplab Tarafder <biplab.tarafder2001@gmail.com>
2023-04-23 23:33:23 +0530

    Navigate to chat screen on clicking chat user card

--- lib/widgets/chat_user_card.dart
+++ lib/widgets/chat_user_card.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:in_chat/main.dart';
 import 'package:in_chat/models/chat_user.dart';
+import 'package:in_chat/screens/chat_screen.dart';
 
 class ChatUserCard extends StatefulWidget {
   final ChatUser user;
@@ -20,7 +21,10 @@ class _ChatUserCardState extends State<ChatUserCard> {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
       elevation: 1.5,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (_) => ChatScreen(user: widget.user)));
+        },
         child: ListTile(
           // leading: const CircleAvatar(
           //   child: Icon(CupertinoIcons.person),

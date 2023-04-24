a2c21917120fec1d7790b511226a1ae57a365ed0
Biplab Tarafder <biplab.tarafder2001@gmail.com>
2023-04-24 21:55:18 +0530

    Change system status bar colour to white

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -23,7 +23,7 @@ class _SplashScreenState extends State<SplashScreen> {
       // set status bar to transparent
       SystemChrome.setSystemUIOverlayStyle(
         const SystemUiOverlayStyle(
-            statusBarColor: Colors.transparent,
+            statusBarColor: Colors.white,
             systemNavigationBarColor: Colors.white),
       );
       // exit from full-screen
